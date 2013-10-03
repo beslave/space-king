4344bba1b9e681e41249ab2d77ebac840458c8c3
Vladislav Lozhechnik <=>
2013-10-03 17:45:34 +0300

    store all info about ship and it's changes for do bot logic

--- space_king/player/bot.py
+++ space_king/player/bot.py
@@ -10,18 +10,20 @@ class Bot(Player):
 
     def __init__(self, *a, **k):
         super(Bot, self).__init__(*a, **k)
-        self.enemy_ship = None
+        self.enemy_ship = {}
+        self.enemy_ship_changes = {}
 
     def in_play(self, status=True):
         super(Bot, self).in_play(status)
         self.next_step()
 
-    def send_ship(self, ship=None):
-        if ship:
-            self.enemy_ship = ship
-        return super(Bot, self).send_ship(ship)
+    def send_ship(self, diff=None):
+        if diff:
+            self.enemy_ship_changes = diff
+            self.enemy_ship.update(diff)
+        return super(Bot, self).send_ship(diff)
 
     def next_step(self):
         if self.is_play:
             self.__command_forward__()
-            reactor.callLater(settings.SYSTEM_DELAY * 2.0, self.next_step)
+            reactor.callLater(settings.SYSTEM_DELAY, self.next_step)
